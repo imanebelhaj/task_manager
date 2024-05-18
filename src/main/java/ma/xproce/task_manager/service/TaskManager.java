c9453a1fddb176b4901b659f25b185fd904d8eba
imane <ibelhaj962@gmail.com>
2024-05-18 01:24:19 +0100

    front end 99%, dasboard add task add list ... (no user profile or update) --starting to work on controllers to connect back with front :) 2

---
+++ src/main/java/ma/xproce/task_manager/service/TaskManager.java
@@ -0,0 +1,19 @@
+package ma.xproce.task_manager.service;
+
+import ma.xproce.task_manager.dao.entites.Task;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+import java.util.List;
+
+@Component
+public interface TaskManager {
+    public Task addTask(Task task);
+    public void deleteTask(Long taskId);
+    public Task updateTask(Task task);
+    public Task updateTaskCompletion(Long taskId, boolean completed);
+    boolean isDeadlinePassed(Date deadline);
+    public Task findTaskById(Long id);
+    public List<Task> getAllTasks();
+
+}
